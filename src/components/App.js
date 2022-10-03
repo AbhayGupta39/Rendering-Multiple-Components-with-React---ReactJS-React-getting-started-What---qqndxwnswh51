955a4de15e4a9fb0e76a075e58aee74db8fc6537
AbhayGupta39 <abhaygupta1400@gmail.com>
2022-10-03 10:35:59 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div id="main">
       <h1 data-ns-test="project-name">Name</h1>
-      <h6 data-ns-test="project-description">Nice</h6>
+      <h3 data-ns-test="project-description">Nice</h3>
     </div>
   )
 }
